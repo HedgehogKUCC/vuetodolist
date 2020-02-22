d0f878f18af504e6bdcb0041b5cb904c815fdafa
HedgehogKU <ccc908925@gmail.com>
2020-02-22 15:58:08 +0800

    完成第一筆假資料

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vue.js Section 3： Todo List
+
+[gh-pages](https://hedgehogkucc.github.io/vuetodolist/)
--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,20 @@ import '../scss/index.scss';
 import '@babel/polyfill';
 
 var app = new Vue({
-  el: '#app'
+  el: '#app',
+  data: {
+    newTodo: '',
+    todos: [
+      {
+        id: '777',
+        title: 'Hello World',
+        completed: false,
+      }
+    ],
+  },
+  methods: {
+    addTodo() {
+      console.log('addTodo');
+    }
+  },
 });
